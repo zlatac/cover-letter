2d0e0dc17c6330fabea9feb5893e5ea56a0bb68d
zlatac <[idahosa1adaghe@hotmail.ca]>
2017-10-19 19:43:25 -0400

    ng build app first lol

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var path = require('path')
 var app = express();
-app.use(express.static(path.resolve(__dirname, 'www')));
-app.set('port', process.env.PORT || 4200);
+app.use(express.static(path.resolve(__dirname, 'dist')));
+app.set('port', process.env.PORT || 3000);
 app.listen(app.get('port'), function() {
  console.log('listening to Port', app.get('port'));
 });

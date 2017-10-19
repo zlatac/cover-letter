20f4a61164ad9c21d8e02239fed76a317fa54b03
zlatac <[idahosa1adaghe@hotmail.ca]>
2017-10-19 19:20:39 -0400

    change port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var path = require('path')
 var app = express();
 app.use(express.static(path.resolve(__dirname, 'www')));
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 4200);
 app.listen(app.get('port'), function() {
  console.log('listening to Port', app.get('port'));
 });

c54dd459e3c45a8ecda425eb1f0344019e93e355
zlatac <[idahosa1adaghe@hotmail.ca]>
2017-10-19 20:21:32 -0400

    trial

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ var express = require('express');
 var path = require('path')
 var app = express();
 app.use(express.static(path.resolve(__dirname, 'dist')));
+app.get('*', (req,res) => {
+	res.sendFile(path.join(__dirname,'dist/index.html'));
+});
 app.set('port', process.env.PORT || 3000);
 app.listen(app.get('port'), function() {
  console.log('listening to Port', app.get('port'));
